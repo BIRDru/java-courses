7168020371e764ac8a5a00928ca243e5b49c99d5
Ilya V. Murovtsev <imurovtsev@tsc.ts>
2017-03-29 16:44:48 +0300

    create Calculate

---
+++ Calculate.java
@@ -0,0 +1,13 @@
+package ru.birdru;
+
+public class Calculate { 
+	public static void main(String[] arg) { 
+		System.out.println("Calculate..."); 
+		int first = Integer.valueOf(arg[0]); 
+		int second = Integer.valueOf(arg[1]); 
+		int sum = first + second;
+		int dev = first / second;
+		System.out.println("Sum : " + sum);
+		System.out.println("Dev : " + dev);
+	} 
+}
